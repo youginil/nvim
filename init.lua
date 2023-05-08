f7a160f4b57239993067cb4b7c347f895ac362fd
youginil <youginil@outlook.com>
2023-05-08 16:32:39 +0800

    remove keymap

--- init.lua
+++ init.lua
@@ -42,8 +42,6 @@ keymap.set({ "n", "v" }, "<Leader><Leader>", function()
 	end
 end, { noremap = true })
 
-keymap.set({ "v" }, "Y", '"+y', { noremap = true })
-
 keymap.set({ "n", "i" }, "<C-s>", function()
 	vim.cmd("w")
 end, { noremap = true })
